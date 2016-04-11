54c68d7c0f2b41624f0f2a6a8ee88300c7a92081
Adam Dempsey <zeus@zeus.local>
2016-04-11 17:19:11 -0500

    First commit.

---
+++ arxivauthorlist.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import requests
+import bs4
+import re
+
+
+print 'Opening link...'
+bowl = requests.get('http://arxiv.org/list/astro-ph/new')
+soup = bs4.BeautifulSoup(bowl.text,'html.parser')
+author_list=[]
+
+entries=soup.find_all('dd')
+authors = [entry.find_next('div',{'class':'list-authors'}).text.split(':')[-1].strip().split(', \n') for entry in entries]
+handles=[[val['href'].split('+')[-1].split('/')[0].lower() for val in entry.find_all('a')] for entry in entries]
+
+authors = [ dict(zip(h,a)) for h,a in zip(handles,authors) ]
+
+titles = [entry.find_next('div',{'class':'list-title'}).text.split(':')[-1].strip() for entry in entries]
+numbers = soup.find_all('span',{'class':'list-identifier'})
+numbers= [re.findall("\d+\.\d+",num.text)[0] for num in numbers]
+
+
+
+
+
+#for a in soup.find_all('a'):
+#    try:
+#        link = a['href']
+#        if '+' in link:
+#            name = link.split('+')[-1].split('/')[0]
+#            author_list.append(name.lower())
+#    except KeyError:
+#        pass
+
+
+
+with open('/Users/zeus/ciera_members/ciera_members.txt','r') as f:
+    ciera=f.readlines()
+
+
+ciera=[line.strip().lower() for line in ciera]
+ciera_authors=[]
+for person in ciera:
+    for auths,title,number in zip(authors,titles,numbers):
+        if person in auths.keys():
+            ciera_authors.append([auths[person],title,number])
+if len(ciera_authors) > 0:
+    print 'Some ciera members released an article today.'
+    for (person,title,number) in ciera_authors:
+        print person + ': ' + number + ', ' + title
+else:
+    print 'Nobody form Ciera posted a paper today.'
+
