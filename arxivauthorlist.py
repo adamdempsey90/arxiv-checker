62383472eb06f513c1bd47e7c47163dbddf0783d
Adam Dempsey <adamdemps@gmail.com>
2016-04-12 09:54:47 -0500

    Added better file access.

--- arxivauthorlist.py
+++ arxivauthorlist.py
@@ -3,6 +3,7 @@
 import requests
 import bs4
 import re
+from os.path import expanduser
 
 
 print 'Opening link...'
@@ -10,20 +11,22 @@ bowl = requests.get('http://arxiv.org/list/astro-ph/new')
 soup = bs4.BeautifulSoup(bowl.text,'html.parser')
 author_list=[]
 
-entries=soup.find_all('dd')
+entries=soup.find_all('dd') # Every new paper is enclosed in <dd> </dd> tags
 authors = [entry.find_next('div',{'class':'list-authors'}).text.split(':')[-1].strip().split(', \n') for entry in entries]
 handles=[[val['href'].split('+')[-1].split('/')[0].lower() for val in entry.find_all('a')] for entry in entries]
 
 authors = [ dict(zip(h,a)) for h,a in zip(handles,authors) ]
 
 titles = [entry.find_next('div',{'class':'list-title'}).text.split(':')[-1].strip() for entry in entries]
+
+# Arxiv numbers are before the <dd> tag
 numbers = soup.find_all('span',{'class':'list-identifier'})
 numbers= [re.findall("\d+\.\d+",num.text)[0] for num in numbers]
 
 
 
-
-with open('/Users/zeus/ciera_members/ciera_members.txt','r') as f:
+# Now we crossreference the author list versus the department
+with open(expanduser('~/ciera_members/ciera_members.txt'),'r') as f:
     ciera=f.readlines()
 
 
@@ -38,5 +41,5 @@ if len(ciera_authors) > 0:
     for (person,title,number) in ciera_authors:
         print person + ': ' + number + ', ' + title
 else:
-    print 'Nobody form Ciera posted a paper today.'
+    print 'Nobody form ciera posted a paper today.'
 
