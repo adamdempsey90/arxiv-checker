688383520e1b6bf38aeb94cdd74139723bba5e21
Adam Dempsey <adamdemps@gmail.com>
2016-06-27 10:27:23 -0500

    Added functionality and updated Readme.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ArxivAuthorList
-Get all of the authors from the most recent mailing of astro-ph
+Get all of the authors from the most recent mailing of arxiv and cross check the results with a list of authors you're interested in.
+
+As an example, say you have a list of authors' arxiv identifiers housed in the file "authors.txt". To check these names against the most recent mailing of the astro-ph arxiv, you would simply do,
+
+git clone https://github.com/adamdempsey90/ArxivAuthorList.git && python ArxivAuthorList/arxivauthorlist.py astro-ph /path/to/file/authors.txt
+
+
+It's also easy to set this script up to run every morning after a new mailing has been sent out using a Cronjob. For example, on my work computer I have a Cronjob that runs an update script every morning at 8 am and emails me the results. To monitor arxiv I added these lines at the end of my update script.   
+
+if [[ $(date +%u) -lt 6 ]]; # Check that it's a weekday
+then
+    cd ~/ArxivAuthorList
+    git pull
+    python arxivauthorlist.py astro-ph /path/to/file/members.txt 
+fi
--- arxivauthorlist.py
+++ arxivauthorlist.py
@@ -3,43 +3,68 @@
 import requests
 import bs4
 import re
-from os.path import expanduser
 
 
-print 'Opening link...'
-bowl = requests.get('http://arxiv.org/list/astro-ph/new')
-soup = bs4.BeautifulSoup(bowl.text,'html.parser')
-author_list=[]
+def check_authors(arxiv_name, author_filename):
+    """
+    Check the new arxiv mailing for the given authors.
+    arxiv_name = The arxiv you're interested in, e.g astro-ph
+    author_filename = text file that contains the arxiv identifiers of
+                    the authors you want to check for. Note that arxiv
+                    tags authors as LASTNAME_FIRSTNAMEINITIAL.
+    """
 
-entries=soup.find_all('dd') # Every new paper is enclosed in <dd> </dd> tags
-authors = [entry.find_next('div',{'class':'list-authors'}).text.split(':')[-1].strip().split(', \n') for entry in entries]
-handles=[[val['href'].split('+')[-1].split('/')[0].lower() for val in entry.find_all('a')] for entry in entries]
+    url_str = 'http://arxiv.org/list/' + arxiv_name + '/new'
 
-authors = [ dict(zip(h,a)) for h,a in zip(handles,authors) ]
+    print 'Checking ', url_str
+    print '\tagainst names contained in ', author_filename
 
-titles = [entry.find_next('div',{'class':'list-title'}).text.split(':')[-1].strip() for entry in entries]
+    bowl = requests.get(url_str)
+    soup = bs4.BeautifulSoup(bowl.text,'html.parser')
 
-# Arxiv numbers are before the <dd> tag
-numbers = soup.find_all('span',{'class':'list-identifier'})
-numbers= [re.findall("\d+\.\d+",num.text)[0] for num in numbers]
+    entries=soup.find_all('dd') # Every new paper is enclosed in <dd> </dd> tags
+    authors = [entry.find_next('div',{'class':'list-authors'}).text.split(':')[-1].strip().split(', \n') for entry in entries]
+    handles=[[val['href'].split('+')[-1].split('/')[0].lower() for val in entry.find_all('a')] for entry in entries]
+
+    authors = [ dict(zip(h,a)) for h,a in zip(handles,authors) ]
+
+    titles = [entry.find_next('div',{'class':'list-title'}).text.split(':')[-1].strip() for entry in entries]
+
+    # Arxiv numbers are before the <dd> tag
+    numbers = soup.find_all('span',{'class':'list-identifier'})
+    numbers= [re.findall("\d+\.\d+",num.text)[0] for num in numbers]
 
 
 
-# Now we crossreference the author list versus the department
-with open(expanduser('~/ciera_members/ciera_members.txt'),'r') as f:
-    ciera=f.readlines()
+    # Now we crossreference the author list versus the department
+
+    try:
+        with open(author_filename,'r') as f:
+            author_list = f.readlines()
+    except IOError:
+        print author_filename, 'could not be found!'
+        raise
 
 
-ciera=[line.strip().lower() for line in ciera]
-ciera_authors=[]
-for person in ciera:
-    for auths,title,number in zip(authors,titles,numbers):
-        if person in auths.keys():
-            ciera_authors.append([auths[person],title,number])
-if len(ciera_authors) > 0:
-    print 'Some ciera members released an article today.'
-    for (person,title,number) in ciera_authors:
-        print person + ': ' + number + ', ' + title
-else:
-    print 'Nobody from ciera posted a paper today.'
+    author_list =[line.strip().lower() for line in author_list]
+    db_authors=[]
+    for person in author_list:
+        for auths,title,number in zip(authors,titles,numbers):
+            if person in auths.keys():
+                db_authors.append([auths[person],title,number])
+    if len(db_authors) > 0:
+        for (person,title,number) in db_authors:
+            print person + ': ' + number + ', ' + title
+    else:
+        print 'No one posted a paper today.'
+if __name__ == "__main__":
+    import sys
 
+    """ For command line arguments:
+        first argument is arxiv name, i.e astro-ph
+        second argument is file name for author list to check against """
+
+    if len(sys.argv) > 2:
+        check_authors(sys.argv[1],sys.argv[2])
+    else:
+        print 'Please supply two arguments.'
