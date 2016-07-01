c7d46d41172e7b91cf983edfbdc728ef8b89206a
Adam Dempsey <zeus@dhcp-10-105-101-0.wireless.northwestern.private>
2016-07-01 15:45:18 -0500

    Pep8 compatible.

--- arxivchecker.py
+++ arxivchecker.py
@@ -49,29 +49,32 @@ class Paper():
         for i in range(howmany):
             if len(self.identifiers) > 0:
                 print 'Removing ' + self.identifiers.pop(-1) \
-                        + ' from identifier list'
+                    + ' from identifier list'
 
     def __str__(self):
         """ Display the paper in a somewhat nice looking way. """
         pad_left = '%%%  '
         pad_right = '  %%%'
+        lp_left = len(pad_left)
+        lp_right = len(pad_right)
+        lp_tot = lp_left + lp_right
         authstr = ', '.join(self.authors)
-        maxlen = max([len(self.title),
-            len(self.link),
-            len(authstr)]) + len(pad_left) + len(pad_right)
+        maxlen = max([len(self.title), len(self.link), len(authstr)]) + lp_tot
 
-        border = ''.join(['%']*maxlen)
-        spacer = pad_left + ''.join([' ']*(maxlen-len(pad_left)-len(pad_right))) + pad_right
+        border = ''.join(['%'] * maxlen)
+        spacer = pad_left + ''.join([' '] * (maxlen - lp_tot)) + pad_right
+        spacer_title = ''.join([' '] * (maxlen - len(self.title) - lp_tot))
+        spacer_link = ''.join([' '] * (maxlen - len(self.link) - lp_tot))
+        spacer_authstr = ''.join([' '] * (maxlen - len(authstr) - lp_tot))
         strbody = '\n\n' + \
-                border + \
-                '\n' + spacer + \
-                '\n' + pad_left + self.title + ''.join([' ']*(maxlen -len(self.title) - len(pad_left) - len(pad_right)))+ pad_right + \
-                '\n' + pad_left + self.link + ''.join([' ']*(maxlen -len(self.link) - len(pad_left) - len(pad_right))) + pad_right + \
-                '\n' + pad_left + authstr + ''.join([' ']*(maxlen -len(authstr) - len(pad_left) - len(pad_right)))+ pad_right + \
-                '\n' + spacer + \
-                '\n' + border + '\n'
+            border + \
+            '\n' + spacer + \
+            '\n' + pad_left + self.title + spacer_title + pad_right + \
+            '\n' + pad_left + self.link + spacer_link + pad_right + \
+            '\n' + pad_left + authstr + spacer_authstr + pad_right + \
+            '\n' + spacer + \
+            '\n' + border + '\n'
         return strbody
-
 
 
 def check_authors(arxiv_names, author_filename):
@@ -81,7 +84,8 @@ def check_authors(arxiv_names, author_filename):
     arxiv_names = The arxivs you're interested in, e.g astro-ph
         To check multiple arxivs include them as a list.
 
-    author_filename = text file that contains the names of the authors you want to check for.
+    author_filename = text file that contains the names of the authors
+                    you want to check for.
 
         The format of the file should be one name per line and Last, First, e.g
 
@@ -89,7 +93,7 @@ def check_authors(arxiv_names, author_filename):
         James, Tom
 
     An example of a call would be,
-        check_authors( ['astro-ph','gr-qc','hep-th','physics'], '~/my_list.txt')
+    check_authors( ['astro-ph','gr-qc','hep-th','physics'], '~/my_list.txt')
 
     """
 
@@ -98,9 +102,9 @@ def check_authors(arxiv_names, author_filename):
         # We have just a single arxiv
         arxiv_names = [arxiv_names]
 
-    authors = [ ]
-    titles = [ ]
-    numbers = [ ]
+    authors = []
+    titles = []
+    numbers = []
     for arxiv_name in arxiv_names:
         url_str = 'http://arxiv.org/list/' + arxiv_name + '/new'
 
@@ -109,19 +113,25 @@ def check_authors(arxiv_names, author_filename):
         bowl = requests.get(url_str)
         soup = bs4.BeautifulSoup(bowl.text, 'html.parser')
 
-        entries=soup.find_all('dd') # Every new paper is enclosed in <dd> </dd> tags
-        authors_t = [entry.find_next('div', {'class':'list-authors'}).text.split(':')[-1].strip().split(', \n') for entry in entries]
-        handles=[[val['href'].split('+')[-1].split('/')[0].lower() for val in entry.find_all('a')] for entry in entries]
+        # Every new paper is enclosed in <dd> </dd> tags
+        entries = soup.find_all('dd')
+        auth_t = [entry.find_next('div', {'class': 'list-authors'}).text.split(
+            ':')[-1].strip().split(', \n') for entry in entries]
+        handles = [[val['href'].split('+')[-1].split('/')[0].lower()
+                    for val in entry.find_all('a')] for entry in entries]
 
-        authors = authors + [ dict(zip(h,a)) for h,a in zip(handles, authors_t) ]
+        authors = authors + [dict(zip(h, a))
+                             for h, a in zip(handles, auth_t)]
 
-        titles = titles + [entry.find_next('div', {'class':'list-title'}).text.split(':')[-1].strip() for entry in entries]
+        titles_t = [entry.find_next('div', {'class': 'list-title'}).text.split(
+            ':')[-1].strip() for entry in entries]
+
+        titles = titles + titles_t
 
         # Arxiv numbers are before the <dd> tag
-        numbers_t = soup.find_all('span', {'class':'list-identifier'})
-        numbers = numbers + [re.findall("\d+\.\d+", num.text)[0] for num in numbers_t]
-
-
+        numbers_t = soup.find_all('span', {'class': 'list-identifier'})
+        numbers = numbers + [re.findall("\d+\.\d+", num.text)[0]
+                             for num in numbers_t]
 
     # Now we crossreference the author list versus the department
 
@@ -132,17 +142,18 @@ def check_authors(arxiv_names, author_filename):
         print author_filename, 'could not be found!'
         raise
 
-
-    author_list =[line.strip().lower() for line in author_list]
+    author_list = [line.strip().lower() for line in author_list]
 
     first_names = [line.split(',')[1].strip().lower() for line in author_list]
-    id_list = [(line.split(',')[0].replace('-','_').strip() + '_' + line.split(',')[1].strip()[0]).lower() for line in author_list]
+    id_list = [(line.split(',')[0].replace('-', '_').strip() + '_' +
+                line.split(',')[1].strip()[0]).lower() for line in author_list]
 
     papers = []
-    for person,id_t  in zip(first_names, id_list):
-        for auths,title,number in zip(authors, titles,numbers):
+    for person, id_t in zip(first_names, id_list):
+        for auths, title, number in zip(authors, titles, numbers):
             if id_t in auths.keys():
-                # Check that the first name matches to avoid non-unique identifiers
+                # Check that the first name matches to avoid non-unique
+                # identifiers
                 if person in auths[id_t].lower():
                     if number not in [paper.number for paper in papers]:
                         # New paper
@@ -152,15 +163,13 @@ def check_authors(arxiv_names, author_filename):
                         papers.append(record)
                     else:
                         # Add this author to the paper
-                        for i,paper in enumerate(papers):
-                            # Make sure we don't add the same author twice to the same paper
+                        for i, paper in enumerate(papers):
+                            # Make sure we don't add the same author twice to
+                            # the same paper
                             if id_t not in paper.identifiers:
-                                if number  == paper.number:
+                                if number == paper.number:
                                     papers[i].add_author(auths[id_t])
                                     papers[i].add_identifier(id_t)
-
-
-
 
     if len(papers) > 0:
         for paper in papers:
@@ -181,4 +190,3 @@ if __name__ == "__main__":
         check_authors(sys.argv[1:-1], sys.argv[-1])
     else:
         print 'Please supply an arxiv name and a list of authors.'
-
